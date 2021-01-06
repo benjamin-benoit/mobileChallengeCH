3d1658f259baf08b6e674623048be8d659cb0841
benjamin-benoit <benjaminbenoitfr@gmail.com>
2021-01-06 16:45:14 +0100

    Address Enthering screen and black theme

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {DarkTheme, NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 import AddressEntering from "./src/components/AddressEntering";
 import QRScanner from "./src/components/QRScanner";
 import TransactionList from "./src/components/TransactionList";
 import TransactionDetails from "./src/components/TransactionDetails";
+import { darkblue } from 'color-name';
 
 const Stack = createStackNavigator()
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="addressEntering" component={AddressEntering} />
-        <Stack.Screen name="QRScanner" component={QRScanner} />
-        <Stack.Screen name="transactionList" component={TransactionList} />
-        <Stack.Screen name="transactionDetails" component={TransactionDetails} />
-      </Stack.Navigator>
-    </NavigationContainer>
+  <ApplicationProvider {...eva} theme={eva.dark}>
+      <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Coinhouse Ethersan" component={AddressEntering} />
+          <Stack.Screen name="QRScanner" component={QRScanner} />
+          <Stack.Screen name="transactionList" component={TransactionList} />
+          <Stack.Screen name="transactionDetails" component={TransactionDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
   </ApplicationProvider>
 );
 
@@ -34,12 +35,16 @@ export default () => (
 //     </View>
 //   );
 // }
+const MyTheme = {
+  ...DarkTheme,
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: 20,
+//   },
+// });
--- src/components/AddressEntering/index.js
+++ src/components/AddressEntering/index.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { StyleSheet } from 'react-native';
+import { Layout, Text, Input, Button } from '@ui-kitten/components';
 
 const AddressEntering = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>Address Entering Screen</Text>
+  <Layout style={{flex: 1, paddingTop: 20, alignItems: 'center'}}>
+    <Text>Please enter a valid ethereum address:</Text>
+    <Input style={styles.input} placeholder='Enter your ethereum address' />
+    <Button style={styles.button} >
+        Lookup
+    </Button>
   </Layout>
 );
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 20,
+  },
+  button: {
+    margin: 2,
+  },
+});
 
 export default AddressEntering;
