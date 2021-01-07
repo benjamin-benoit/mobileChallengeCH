23843527b1f51cf152b2a943289c351d785894fa
benjamin-benoit <benjaminbenoitfr@gmail.com>
2021-01-07 01:09:55 +0100

    get transactions

--- src/components/AddressEntering/index.js
+++ src/components/AddressEntering/index.js
@@ -1,42 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, ActivityIndicator, TouchableOpacity, View } from 'react-native';
-import { Layout, Text, Input, Button } from '@ui-kitten/components';
+import { Layout, Text } from '@ui-kitten/components';
 import { FlatList } from "react-native-gesture-handler";
 import axios from 'axios';
 
+
+import AddressForm from "../AddressForm";
+
 const AddressEntering = ({navigation}) => {
 
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+    let transactions;
 
-    useEffect(() => {
-        axios.get('https://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc&apikey=3C3TV9PS1H3GH31ZW115CKCN95U2PGZGMU&address=0xf7eB7637DeD2696B152c7D5EDEe502902B0F1c91')
-        .then(({ data }) => {
-            console.log("defaultApp -> data", data.result)
-            setData(data.result)
-        })
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    }, []);
+    const onAddressChange = (data) => {
+        transactions = data;
+        console.log(transactions)
+    };
 
     return (
         <Layout style={{flex: 1, paddingTop: 20, alignItems: 'center'}}>
             <Text>Please enter a valid ethereum address:</Text>
-            {/* {transactions && transactions.map((transaction) => {
-                return <Text key={transaction.blockNumber}>{transaction.blockHash}</Text>;
-            })} */}
-            <Input style={styles.input} placeholder='Enter your ethereum address' />
-            <Button style={styles.button} onPress={() => {
-                navigation.navigate('transactionDetails')
-            }}>
-                Lookup
-            </Button>
-            {isLoading ? <ActivityIndicator /> : (
+            <AddressForm onAddressChange={onAddressChange} />
                 <FlatList
-                style={{width: '100%'}}
-                data={data}
+                style={styles.flatlist}
+                data={transactions}
                 keyExtractor={(item, index) => {
-                    // console.log("index", index)
                     return index.toString();
                 }}
                 renderItem={({ item }) => {
@@ -51,17 +38,15 @@ const AddressEntering = ({navigation}) => {
                     )
                 }}
                 />
-            )}
         </Layout>
     );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    margin: 20,
-  },
-  button: {
-    margin: 2,
+  flatlist: {
+    width: '100%',
+    marginTop: 30,
+    backgroundColor: '#222A45'
   },
   itemView: {
     padding: 10,
